c21d3c600a57c65af5ebd5661f956b7fc3bce2f7
Ankush Rai <113849948+AnkushRai-222@users.noreply.github.com>
2022-10-18 18:18:00 +0530

    Update formatter.js

--- src/validatorFile/formatter.js
+++ src/validatorFile/formatter.js
@@ -1,11 +1,12 @@
-let trim = function(){
+
+let trim1= function(){
     let arr =["Ankush","rai","Bhopal","Madhya Pradesh"]
     let val = arr.map(el =>{
         return el.toLowerCase();
     })
     return val;
 }
-let trim1 = function(){
+let trim2 = function(){
     let arr =["Ankush","rai","Bhopal","Madhya Pradesh"]
     let val1 = arr.map(el =>{
         return el.toUpperCase();
@@ -13,5 +14,5 @@ let trim1 = function(){
     return val1;
 }
 
-module.exports.mytrim = trim
 module.exports.mytrim1 = trim1
+module.exports.mytrim2 = trim2
