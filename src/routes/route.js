3b48d98c9c687bd40d743432ac90dd167a3d1ac9
AnkushRai-222 <ankushrai222@gmail.com>
2022-10-19 21:50:55 +0530

    Assingmnet of post Api

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,73 +1,82 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/students/:name', function(req, res) {
-    let studentName = req.params.name
-    console.log(studentName)
-    res.send(studentName)
+//assingment 1st 
+router.get('/sol1',function(req,res){
+    let arr =[ 1,2,3,5,6];
+    let n = arr.length+1;
+    let total = n*(n+1)/2
+    let sum =0;
+    for(let i =0 ;i<arr.length;i++){
+        sum = sum + arr[i];
+    }
+    let missing = total - sum ;
+    res.send({missingNumber:missing});
+})
+// asssingment 2nd
+router.get('/sol2',function(req,res){
+    let array = [33,34,35,37,38,39]
+    let n = array.length+1;
+    let first = array[0];
+    let last = array[array.length-1]
+
+    var total = n*(first+last)
+    total = total/2;
+    let output = 0;
+
+    for(let i = 0 ; i<array.length;i++){
+        output = output+array[i];
+    }
+    let missing = (total - output);
+
+    res.send({missingNumber:missing})
 })
 
-router.get("/random" , function(req, res) {
-    res.send("hi there")
+// Assingment of the day 
+
+let players =
+   [
+       {
+           "name": "manish",
+           "dob": "1/1/1995",
+           "gender": "male",
+           "city": "jalandhar",
+           "sports": [
+               "swimming"
+           ]
+       },
+       {
+        "name": "gopal",
+        "dob": "1/09/1995",
+        "gender": "male",
+        "city": "delhi",
+        "sports": [
+            "soccer"
+        ]
+    },
+    {
+        "name": "lokesh",
+        "dob": "1/1/1990",
+        "gender": "male",
+        "city": "mumbai",
+        "sports": [
+            "soccer"
+        ]
+    },
+]
+
+router.post('/players',function(req,res){
+    let body = req.body;
+
+        players.push(body)
+        res.send(  { data: players , status: true }  )
+
+    
+
+
+
 })
 
 
-router.get("/test-api" , function(req, res) {
-    res.send("hi FunctionUp")
-})
-
-
-router.get("/test-api-2" , function(req, res) {
-    res.send("hi FunctionUp. This is another cool API")
-})
-
-
-router.get("/test-api-3" , function(req, res) {
-    res.send("hi FunctionUp. This is another cool API. And NOw i am bored of creating API's ")
-})
-
-
-router.get("/test-api-4" , function(req, res) {
-    res.send("hi FunctionUp. This is another cool API. And NOw i am bored of creating API's. PLZ STOP CREATING MORE API;s ")
-})
-
-
-
-router.get("/test-api-5" , function(req, res) {
-    res.send("hi FunctionUp5. This is another cool API. And NOw i am bored of creating API's. PLZ STOP CREATING MORE API;s ")
-})
-
-router.get("/test-api-6" , function(req, res) {
-    res.send({a:56, b: 45})
-})
-
-router.post("/test-post", function(req, res) {
-    res.send([ 23, 45 , 6])
-})
-
-
-router.post("/test-post-2", function(req, res) {
-    res.send(  { msg: "hi" , status: true }  )
-})
-
-router.post("/test-post-3", function(req, res) {
-    // let id = req.body.user
-    // let pwd= req.body.password
-
-    // console.log( id , pwd)
-
-    console.log( req.body )
-
-    res.send(  { msg: "hi" , status: true }  )
-})
-
-
-
-router.post("/test-post-4", function(req, res) {
-    let arr= [ 12, "functionup"]
-    let ele= req.body.element
-    arr.push(ele)
-    res.send(  { msg: arr , status: true }  )
-})
 
 module.exports = router;
