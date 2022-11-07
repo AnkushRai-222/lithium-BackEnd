9e5ab7392cfb6de7307370380791223bc0c241b5
AnkushRai-222 <ankushrai222@gmail.com>
2022-11-07 18:41:02 +0530

    assingment

--- src/controllers/newUsercontroller.js
+++ src/controllers/newUsercontroller.js
@@ -2,21 +2,26 @@ const newUserModel = require('../models/newUserModel');
 const jwt = require('jsonwebtoken')
 
 const createUser = async function(req,res){
+  try{
     let body = req.body;
     let savedata = await newUserModel.create(body);
-    res.send({status:true,msg:savedata});
-
+    res.status(201).send({status:true,msg:savedata});
+}
+catch(error){
+  return res.status(500).send({status:false,error:error.message})
+}
 }
 module.exports.createUserNew=createUser
 
 
 const userLogin = async function(req,res){
+  try{
     const userEmail = req.body.emailId;
     const userPassword = req.body.password;
     
     const findUser = await newUserModel.findOne({emailId:userEmail,password:userPassword});
     if(!findUser){
-        return res.send({status:false,msg:"email or password is incorrect"})
+        return res.status(400).send({status:false,msg:"email or password is incorrect"})
     }
     let mytoken = jwt.sign(
         {
@@ -27,39 +32,55 @@ const userLogin = async function(req,res){
         'functionUp-lithium'
     );
     res.setHeader("x-auth-token",mytoken)
-    res.send({status:true,data:mytoken});
-    
+    res.status(201).send({status:true,data:mytoken});
+ }  
+ catch(error){
+  return res.status(500).send({status:false,error:error.message})
+} 
 }
 module.exports.userLogin=userLogin
 
 const userDetail = async function(req,res){
-
+try{
     let userId = req.params.userId;
     let user = await newUserModel.findById(userId)
-    if(!user) return res.send({status:false,msg:"user not found/invalid user id"})
-    res.send({status:true,data:user});
+    if(!user) return res.status(400).send({status:false,msg:"user not found/invalid user id"})
+    res.status(200).send({status:true,data:user});
+  } 
+  catch(error){
+    return res.status(500).send({status:false,error:error.message})
+} 
 }
 module.exports.userDetail=userDetail
 
 const updateUser = async function(req,res){
-
+try{
     let userId = req.params.userId;
     let user = await newUserModel.findById(userId)
-    if(!user) return res.send({status:false,msg:"user not found/invalid user id"})
+    if(!user) return res.status(400).send({status:false,msg:"user not found/invalid user id"})
     const body = req.body
     let updateUser = await newUserModel.findOneAndUpdate({_id:userId},body,{new:true})
-    res.send({status:true,updatedData:updateUser});
+    res.status(200).send({status:true,updatedData:updateUser});
+ }   
+ catch(error){
+  return res.status(500).send({status:false,error:error.message})
+}
 }
 
 module.exports.updateUser = updateUser;
 
 const deleteuser = async function(req,res){
+  try{
     let userId = req.params.userId;
     let user = await newUserModel.findById(userId);
     if(!user){
-        return res.send({status:false,msg:"user not found/invalid user id"})
+        return res.status(400).send({status:false,msg:"user not found/invalid user id"})
     }
     let deleteVal = await newUserModel.findOneAndUpdate({_id:userId},{isDeleted:true},{new:true})
-    res.send({status:true,DeletedData:deleteVal});
+    res.status(200).send({status:true,DeletedData:deleteVal});
+  }  
+  catch(error){
+    return res.status(500).send({status:false,error:error.message})
+}
 }
 module.exports.deleteuser = deleteuser;
--- src/middleware/authenCheck.js
+++ src/middleware/authenCheck.js
@@ -1,15 +1,20 @@
 const jwt = require('jsonwebtoken')
 
 const authCheck = function(req,res,next){
+    try{
 const headerTokan = req.headers["x-auth-token"]
 if(!headerTokan){
-    return res.send({status:false,msg:"header must be included"})
+    return res.status(400).send({status:false,msg:"header must be included"})
 }
 let decoded = jwt.verify(headerTokan,'functionUp-lithium');
 if(!decoded){
-    return res.send({status : false,msg:"tokan is invalid"});
+    return res.status(401).send({status : false,msg:"tokan is invalid"});
 
 }
 next()
+}
+catch(error){
+    return res.status(500).send({status:false,error:error.message})
+}
 }
 module.exports.Authencheck = authCheck
